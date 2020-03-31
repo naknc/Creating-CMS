371b35eb677f71cf66c166072bfe25f5e8fdf5c3
naknc <nihan.akinci.35@gmail.com>
2020-03-31 16:48:04 +0300

    urunler modulünün ürün listesi için ürünler şablonu oluşturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,33 @@
-ürün listesi content
+<div class="row">
+<div class="col-md-12">
+				<h4 class="m-b-lg">Ürün Listesi</h4>
+			</div><!-- END column -->
+<div class="col-md-12">
+				<div class="widget p-lg">
+					<table class="table table-hover table-striped">
+						
+						<thead>
+							<th>#id</th>
+							<th>url</th>
+							<th>Başlık</th>
+							<th>Açıklama</th>
+							<th>Durumu</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>#1</td>
+								<td>monitor-askisi</td>
+								<td>Monitör Askısı</td>
+								<td>360 dereve kullanılabilen monitör askısıdır..</td>
+								<td>1/0</td>
+								<td>
+									<a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+									<a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+								</td>
+							</tr>
+						</tbody>
+
+					</table>
+				</div><!-- .widget -->
+			</div>
+</div>
