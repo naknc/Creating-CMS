9a900344fc2ebb4df5a9f219ed3bb2b78ea8fbaf
naknc <nihan.akinci.35@gmail.com>
2020-04-02 13:35:46 +0300

    sweetalert entegre edildi.

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+      
+})
