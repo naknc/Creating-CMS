1979b6600a6ec0a1f14e451cf04e61e5ab7e52c1
naknc <nihan.akinci.35@gmail.com>
2020-04-02 13:57:39 +0300

    SweetAlert ile Confirm kullanılarak kayıt silme işlemi gerçekleştirildi.

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
 
-    Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
-      })
-      
+    $(".remove-btn").click(function(e){
+
+        var $data_url = $(this).data("url");
+
+        Swal.fire({
+            title: 'Emin misiniz?',
+            text: "Bu işlemi geri alamayacaksınız!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil!',
+            cancelButtonText : "Hayır"
+          }).then(function(result){
+            if (result.value) {
+              
+                window.location.href = $data_url;
+            
+            }
+          });
+    })
 })
