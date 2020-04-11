e302a1e2c884846d470ceeed2b8b3ed1932ce3cf
naknc <nihan.akinci.35@gmail.com>
2020-04-11 17:58:55 +0300

    Güncelleme işleminin yapımı.

--- panel/application/controllers/News.php
+++ panel/application/controllers/News.php
@@ -205,7 +205,7 @@ class News extends CI_Controller {
 
     }
 
-    public function update($id){
+    public function update_($id){
         $this->load->library("form_validation");
 
         //kurallar yazılır..
@@ -285,6 +285,141 @@ class News extends CI_Controller {
         
     }
 
+    public function update($id){
+        $this->load->library("form_validation");
+
+        //kurallar yazılır..
+        $news_type = $this->input->post("news_type");
+        
+        if($news_type == "video"){
+
+            $this->form_validation->set_rules("video_url","Video URL","required|trim");
+        }
+
+        $this->form_validation->set_rules("title","Başlık","required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required" => "<b> {field} </b> alanı doldurulmalıdır"
+            )
+        );
+
+        //Form validation çalıştırılır
+        $validate = $this->form_validation->run();
+
+        if($validate){
+            
+            if($news_type == "image"){
+
+                //upload süreci...
+                if($_FILES["img_url"]["name"]!==""){
+                    $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
+
+                    $config["allowed_types"] = "jpg|jpeg|png";
+                    $config["upload_path"] = "uploads/$this->viewFolder/";
+                    $config["file_name"] = $file_name;
+                    
+                    $this->load->library("upload", $config);
+        
+                    $upload = $this->upload->do_upload("img_url");
+        
+                if($upload){
+        
+                    $uploaded_file = $this->upload->data("file_name");
+                    
+                    $data = array(
+                        "title"         => $this->input->post("title"),
+                        "description"   => $this->input->post("description"),
+                        "url"           => convertToSEO($this->input->post("title")),
+                        "news_type"    => $news_type,
+                        "img_url"       => $uploaded_file,
+                        "video_url"     => "#"
+                    ); 
+        
+                } else {
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text"  => "Görsel yüklenirken bir problem oluştu",
+                        "type"  => "error"
+                    );
+        
+                    //İşlemin Sonucunu Session'a yazma işlemi...
+                    $this->session->set_flashdata("alert", $alert);
+        
+                    redirect(base_url("news/update_form/$id"));
+        
+                    die();
+                }
+                } else {
+
+                    $data = array(
+                        "title"         => $this->input->post("title"),
+                        "description"   => $this->input->post("description"),
+                        "url"           => convertToSEO($this->input->post("title"))
+                    ); 
+
+                }
+
+            } else if($news_type == "video"){
+                $data = array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSEO($this->input->post("title")),
+                    "news_type"    => $news_type,
+                    "img_url"       => "#",
+                    "video_url"     => $this->input->post("video_url")
+                ); 
+            }
+
+            $update = $this->news_model->update(array("id" => $id ), $data);
+
+            //TODO Alert sistemi eklenecek...
+            if($update){
+
+                $alert = array(
+                    "title" => "İşlem Başarılı",
+                    "text"  => "Kayıt başarılı bir şekilde güncellendi",
+                    "type"  => "success"
+                );
+
+            } else {
+
+                $alert = array(
+                    "title" => "İşlem Başarısız",
+                    "text"  => "Kayıt güncelleme sırasında bir problem oluştu",
+                    "type"  => "error"
+                );
+
+            }
+
+            //İşlemin Sonucunu Session'a yazma işlemi...
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("news"));
+
+        } else {
+
+            $viewData = new stdClass();
+        
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "update";
+        $viewData->form_error = true;
+        $viewData->news_type = $news_type;
+
+        /**Tablodan Verilerin Getirilmesi.. */
+        $viewData->item = $this->news_model->get(
+            array(
+                "id"        => $id
+            )
+        );
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+        }
+        
+    }
+
     public function delete($id){
         $delete = $this->news_model->delete(
             array(
@@ -313,7 +448,7 @@ class News extends CI_Controller {
 
         $this->session->set_flashdata("alert", $alert);
 
-        redirect(base_url("news"));
+        redirect(base_url("product"));
     }
 
     public function isActiveSetter($id){
@@ -330,6 +465,73 @@ class News extends CI_Controller {
                     "isActive" => $isActive
                 )
             );
+
+        }
+
+    }
+
+    public function imageIsActiveSetter($id){
+        
+        if($id){
+
+            $isActive = ($this->input->post("data") === "true") ? 1 : 0;
+
+            $this->product_image_model->update(
+                array(
+                    "id" => $id
+                ),
+                array(
+                    "isActive" => $isActive
+                )
+            );
+
+        }
+
+    }
+
+    public function isCoverSetter($id, $parent_id){
+        
+        if($id && $parent_id){
+
+            $isCover = ($this->input->post("data") === "true") ? 1 : 0;
+
+            // Kapak yapılmak istenen kayıt
+            $this->product_image_model->update(
+                array(
+                    "id"         => $id,
+                    "product_id" => $parent_id
+                ),
+                array(
+                    "isCover" => $isCover
+                )
+            );
+
+            // Kapak yapılmayan diğer kayıtlar
+            $this->product_image_model->update(
+                array(
+                    "id !="      => $id,
+                    "product_id" => $parent_id
+                ),
+                array(
+                    "isCover" => 0
+                )
+            );
+
+            $viewData = new stdClass();
+        
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "image";
+
+        $viewData->item_images = $this->product_image_model->get_all(
+            array(
+                "product_id" => $parent_id
+            ),  "rank ASC"
+        );
+
+        $render_html = $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/render_elements/image_list_v", $viewData, true);
+
+        echo $render_html;
 
         }
 
