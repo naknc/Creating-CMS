5d4d457c5e4881b565aa87a7e83fc3303e26c472
naknc <nihan.akinci.35@gmail.com>
2020-04-11 13:55:05 +0300

    Haber listesinde resim ve video gösterimi

--- panel/application/views/news_v/list/content.php
+++ panel/application/views/news_v/list/content.php
@@ -38,7 +38,24 @@
 								<td><?php echo $item->url; ?></td>
 								<td><?php echo $item->description; ?></td>
 								<td><?php echo $item->news_type; ?></td>
-								<td>Görsel Gelecek...</td>
+								<td>
+									<?php if($item->news_type == "image") { ?>
+										
+										<img width="100" src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>" 
+											alt="" 
+											class="img_rounded">
+
+									<?php } else if($item->news_type == "video") { ?>
+										<iframe 
+										width="100"
+											src="<?php echo $item->video_url; ?>" 
+											frameborder="0" 
+											allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+											allowfullscreen>
+										</iframe>
+									<?php } ?>
+
+								</td>
 								<td>
 									<input
 										data-url="<?php echo base_url("news/isActiveSetter/$item->id")?>"
