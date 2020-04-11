f409dc7b1e55e47598b2f3286c80ed8e3146f72e
naknc <nihan.akinci.35@gmail.com>
2020-04-11 18:03:14 +0300

    Listeleme sırasında görünüm değişikliği.

--- panel/application/views/news_v/list/content.php
+++ panel/application/views/news_v/list/content.php
@@ -22,7 +22,7 @@
 							<th class="w50">#id</th>
 							<th>Başlık</th>
 							<th>url</th>
-							<th>Açıklama</th>
+							<!--<th>Açıklama</th>-->
 							<th>Haber Türü</th>
 							<th>Görsel</th>
 							<th>Durumu</th>
@@ -36,19 +36,19 @@
 								<td class="w50 text-center"><?php echo $item->id; ?></td>
 								<td><?php echo $item->title; ?></td>
 								<td><?php echo $item->url; ?></td>
-								<td><?php echo $item->description; ?></td>
+								<!--<td><?php echo $item->description; ?></td>-->
 								<td><?php echo $item->news_type; ?></td>
 								<td>
 									<?php if($item->news_type == "image") { ?>
 										
-										<img width="100" 
+										<img width="75" 
 										src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>" 
 											alt="" 
 											class="img_rounded">
 
 									<?php } else if($item->news_type == "video") { ?>
 										<iframe 
-											width="100"
+											width="75"
 											src="<?php echo $item->video_url; ?>" 
 											frameborder="0" 
 											allow="encrypted-media" 
