237d7341ae95d165b06113c775fd5a8bcd9e9ef6
naknc <nihan.akinci.35@gmail.com>
2020-05-05 15:35:23 +0300

    DateTimePicker kullanarak Yeni Kayıt ekleme işleminin yapılması.

--- panel/application/views/courses_v/add/content.php
+++ panel/application/views/courses_v/add/content.php
@@ -1,13 +1,13 @@
 <div class="row">
 	<div class="col-md-12">
 		<h4 class="m-b-lg">
-			Yeni Referans Ekle
+			Yeni Eğitim Ekle
 		</h4>
 	</div><!-- END column -->
 	<div class="col-md-12">
 	<div class="widget">
 		<div class="widget-body">
-			<form action="<?php echo base_url("references/save");?>" method="post" enctype="multipart/form-data">
+			<form action="<?php echo base_url("courses/save");?>" method="post" enctype="multipart/form-data">
 				
 				<div class="form-group">
 					<label>Başlık</label>
@@ -21,14 +21,23 @@
 					<label>Açıklama</label>
 					<textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
 				</div>
-					
-					<div class="form-group image_upload_container">
+
+				<div class="row">
+					<div class="col-md-4">
+						<label for="datetimepicker1">Eğitim Tarihi</label>
+					<input type="hidden" name="event_date" id="datetimepicker1" data-plugin="datetimepicker" data-options="{ inline: true, viewMode: 'days', format : 'YYYY-MM-DD HH:mm:ss' }">
+				</div><!-- END column -->
+
+				<div class="form-group image_upload_container col-md-8">
 					<label for="exampleInputFile">Görsel Seçiniz</label>
-						<input type="file" name="img_url" class="form-control">
+					<input type="file" name="img_url" class="form-control">
 				</div>
+					</div>
+
+				
 				
 				<button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
-				<a href="<?php echo base_url("references");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+				<a href="<?php echo base_url("courses");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
 
 			</form>
 		</div><!-- .widget-body -->
