a9223faae8d7e5587bf729a190743da0bfb7289a
naknc <nihan.akinci.35@gmail.com>
2020-05-28 13:54:11 +0300

    Update işleminin gerçekleştirilmesi.

--- panel/application/controllers/Users.php
+++ panel/application/controllers/Users.php
@@ -67,7 +67,8 @@ class Users extends CI_Controller {
 
                 //upload süreci...
 
-                $insert = $this->user_model->add(array(
+                $insert = $this->user_model->add(
+                    array(
                 "user_name" => $this->input->post("user_name"),
                 "full_name" => $this->input->post("full_name"),
                 "email"     => $this->input->post("email"),
@@ -130,113 +131,90 @@ class Users extends CI_Controller {
     }
 
     public function update($id){
+
         $this->load->library("form_validation");
 
-        //kurallar yazılır..
+        $oldUser = $this->user_model->get (
+                array(
+                    "id" => $id
+                )
+            );
+
+        if($oldUser->user_name != $this->input->post("user_name")){
+            $this->form_validation->set_rules("user_name","Kullanıcı Adı","required|trim|is_unique[users.user_name]");
+        }
+        if($oldUser->email != $this->input->post("email")){
+            $this->form_validation->set_rules("email","E-posta","required|trim|valid_email|is_unique[users.email]");
+        }
         
-        $this->form_validation->set_rules("title","Başlık","required|trim");
+        $this->form_validation->set_rules("full_name","Ad Soyad","required|trim");
 
         $this->form_validation->set_message(
             array(
-                "required" => "<b> {field} </b> alanı doldurulmalıdır"
+                "required"    => "<b> {field} </b> alanı doldurulmalıdır",
+                "valid_email" => "Lütfen geçeril bir e-posta adresi giriniz.",
+                "is_unique"   => "<b>{field}</b> alanı daha önceden kullanılmış.",
             )
         );
-
+        
         //Form validation çalıştırılır
         $validate = $this->form_validation->run();
 
         if($validate){
             
             //upload süreci...
-            if($_FILES["img_url"]["name"]!==""){
-                $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
 
-                $config["allowed_types"] = "jpg|jpeg|png";
-                $config["upload_path"] = "uploads/$this->viewFolder/";
-                $config["file_name"] = $file_name;
-                    
-                $this->load->library("upload", $config);
-        
-                $upload = $this->upload->do_upload("img_url");
-        
-                if($upload){
-        
-                    $uploaded_file = $this->upload->data("file_name");
-                    
-                    $data = array(
-                        "title"         => $this->input->post("title"),
-                        "description"   => $this->input->post("description"),
-                        "url"           => convertToSEO($this->input->post("title")),
-                        "img_url"       => $uploaded_file
-                    ); 
-        
-                } else {
-                    $alert = array(
-                        "title" => "İşlem Başarısız",
-                        "text"  => "Görsel yüklenirken bir problem oluştu",
-                        "type"  => "error"
-                    );
-        
-                    //İşlemin Sonucunu Session'a yazma işlemi...
-                    $this->session->set_flashdata("alert", $alert);
-        
-                    redirect(base_url("references/update_form/$id"));
-        
-                    die();
-                }
+            $update = $this->user_model->update(
+                array("id" => $id ), 
+                array(
+                    "user_name" => $this->input->post("user_name"),
+                    "full_name" => $this->input->post("full_name"),
+                    "email"     => $this->input->post("email")
+                    )
+                );
+
+            //TODO Alert sistemi eklenecek...
+            if($update){
+
+                $alert = array(
+                    "title" => "İşlem Başarılı",
+                    "text"  => "Kayıt başarılı bir şekilde güncellendi",
+                    "type"  => "success"
+                );
+
             } else {
 
-                $data = array(
-                    "title"         => $this->input->post("title"),
-                    "description"   => $this->input->post("description"),
-                    "url"           => convertToSEO($this->input->post("title"))
-                    ); 
+                $alert = array(
+                    "title" => "İşlem Başarısız",
+                    "text"  => "Kayıt güncelleme sırasında bir problem oluştu",
+                    "type"  => "error"
+                );
 
-                }
+            }
 
-        }
+            //İşlemin Sonucunu Session'a yazma işlemi...
+            $this->session->set_flashdata("alert", $alert);
 
-        $update = $this->user_model->update(array("id" => $id ), $data);
-
-        //TODO Alert sistemi eklenecek...
-        if($update){
-
-            $alert = array(
-                "title" => "İşlem Başarılı",
-                "text"  => "Kayıt başarılı bir şekilde güncellendi",
-                "type"  => "success"
-            );
+            redirect(base_url("users"));
 
         } else {
-
-            $alert = array(
-                "title" => "İşlem Başarısız",
-                "text"  => "Kayıt güncelleme sırasında bir problem oluştu",
-                "type"  => "error"
-            );
-
-        }
-
-        //İşlemin Sonucunu Session'a yazma işlemi...
-        $this->session->set_flashdata("alert", $alert);
-
-        redirect(base_url("references"));
-
             $viewData = new stdClass();
         
-        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-        $viewData->viewFolder = $this->viewFolder;
-        $viewData->subViewFolder = "update";
-        $viewData->form_error = true;
+            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "update";
+            $viewData->form_error = true;
 
-        /**Tablodan Verilerin Getirilmesi.. */
-        $viewData->item = $this->user_model->get(
-            array(
-                "id"        => $id
-            )
-        );
+            /**Tablodan Verilerin Getirilmesi.. */
+            $viewData->item = $this->user_model->get(
+                array(
+                    "id"        => $id
+                )
+            );
+        
 
-        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
         
     }
 
