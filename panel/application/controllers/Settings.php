99ae848cbacbaf92fa598e5d11ad28c651b31291
naknc <nihan.akinci.35@gmail.com>
2020-05-31 21:03:27 +0300

    Ayar ekleme işleminin yapımı.

--- panel/application/controllers/Settings.php
+++ panel/application/controllers/Settings.php
@@ -47,75 +47,126 @@ class Settings extends CI_Controller {
     }
 
     public function save(){
+        
         $this->load->library("form_validation");
 
         //kurallar yazılır..
-        $this->form_validation->set_rules("user_name","Kullanıcı Adı","required|trim|is_unique[users.user_name]");
-        $this->form_validation->set_rules("full_name","Ad Soyad","required|trim");
-        $this->form_validation->set_rules("email","E-posta","required|trim|valid_email|is_unique[users.email]");
-        $this->form_validation->set_rules("password","Şifre","required|trim|min_length[6]|max_length[8]");
-        $this->form_validation->set_rules("re_password","Şifre Tekrar","required|trim|min_length[6]|max_length[8]|matches[password]");
+        if($_FILES["logo"]["name"] == ""){
+                
+             $alert = array(
+                "title" => "İşlem Başarısız",
+                "text"  => "Lütfen bir görsel seçiniz",
+                "type"  => "error"
+            );
+
+            //İşlemin Sonucunu Session'a yazma işlemi...
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("settings/new_form"));
+
+            die();
+        }
+
+        $this->form_validation->set_rules("company_name","Şirket Adı","required|trim");
+        $this->form_validation->set_rules("phone_1","Telefon 1","required|trim");
+        $this->form_validation->set_rules("email","E-posta Adresi","required|trim|valid_email");
 
         $this->form_validation->set_message(
             array(
-                "required"    => "<b> {field} </b> alanı doldurulmalıdır",
-                "valid_email" => "Lütfen geçeril bir e-posta adresi giriniz.",
-                "is_unique"   => "<b>{field}</b> alanı daha önceden kullanılmış.",
-                "matches"     => "Şifreler birbirlerini tutmuyor.",
-                "min_length"  => "Şifre en az 6 karakter olmalıdır.",
-                "max_length"  => "Şifre 8 karakterden fazla olamaz."
+                "required" => "<b> {field} </b> alanı doldurulmalıdır",
+                "valid_email" => "Lütfen geçerli bir <b> {field} </b> giriniz"
+            )
+        );
 
-            ));
+        //Form validation çalıştırılır
+        $validate = $this->form_validation->run();
 
-            //Form validation çalıştırılır
-            $validate = $this->form_validation->run();
-
-            if($validate){
+        if($validate){
 
                 //upload süreci...
 
+            $file_name = convertToSEO($this->input->post("company_name")) . "." . pathinfo($_FILES["logo"]["name"], PATHINFO_EXTENSION);
+
+            $config["allowed_types"] = "jpg|jpeg|png";
+            $config["upload_path"] = "uploads/$this->viewFolder/";
+            $config["file_name"] = $file_name;
+            
+            $this->load->library("upload", $config);
+
+            $upload = $this->upload->do_upload("logo");
+
+            if($upload){
+
+                $uploaded_file = $this->upload->data("file_name");
+
                 $insert = $this->settings_model->add(
                     array(
-                "user_name" => $this->input->post("user_name"),
-                "full_name" => $this->input->post("full_name"),
-                "email"     => $this->input->post("email"),
-                "password"  => md5($this->input->post("password")),
-                "isActive"  => 1,
-                "createdAt" => date("Y-m-d H:i:s")
-                )
-            );
-
-            //TODO Alert sistemi eklenecek...
-            if($insert){
-
-                $alert = array(
-                    "title" => "İşlem Başarılı",
-                    "text"  => "Kayıt başarılı bir şekilde eklendi",
-                    "type"  => "success"
-                ); } else {
-
-                $alert = array(
-                    "title" => "İşlem Başarısız",
-                    "text"  => "Kayıt ekleme sırasında bir problem oluştu",
-                    "type"  => "error"
+                    "company_name"  => $this->input->post("company_name"),
+                    "phone_1"       => $this->input->post("phone_1"),
+                    "phone_2"       => $this->input->post("phone_2"),
+                    "fax_1"         => $this->input->post("fax_1"),
+                    "fax_2"         => $this->input->post("fax_2"),
+                    "address"       => $this->input->post("address"),
+                    "about_us"      => $this->input->post("about_us"),
+                    "mission"       => $this->input->post("mission"),
+                    "vision"        => $this->input->post("vision"),
+                    "email"         => $this->input->post("email"),
+                    "facebook"      => $this->input->post("facebook"),
+                    "twitter"       => $this->input->post("twitter"),
+                    "instagram"     => $this->input->post("instagram"),
+                    "linkedin"      => $this->input->post("linkedin"),
+                    "logo"          => $uploaded_file,
+                    "createdAt"     => date("Y-m-d H:i:s")
+                    )
                 );
+
+                //TODO Alert sistemi eklenecek...
+                if($insert){
+
+                    $alert = array(
+                        "title" => "İşlem Başarılı",
+                        "text"  => "Kayıt başarılı bir şekilde eklendi",
+                        "type"  => "success"
+                    );
+
+                } else {
+
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text"  => "Kayıt ekleme sırasında bir problem oluştu",
+                        "type"  => "error"
+                    );
+                }
+
+            } else {
+                    $alert = array(
+                        "title" => "İşlem Başarısız",
+                        "text"  => "Görsel yüklenirken bir problem oluştu",
+                        "type"  => "error"
+                    );
+
+                    $this->session->set_flashdata("alert", $alert);
+
+                    redirect(base_url("settings/new_form"));
+
+                    die();
+
             }
-            
+
             $this->session->set_flashdata("alert", $alert);
 
             redirect(base_url("settings"));
 
-            die();} else {
-                $viewData = new stdClass();
-            
-                /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
-                $viewData->viewFolder = $this->viewFolder;
-                $viewData->subViewFolder = "add";
-                $viewData->form_error = true;
+        } else {
+            $viewData = new stdClass();
+                
+            /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "add";
+            $viewData->form_error = true;
 
-                $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
-
-            } 
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
     }
 
     public function update_form($id){
@@ -170,6 +221,7 @@ class Settings extends CI_Controller {
         if($oldUser->user_name != $this->input->post("user_name")){
             $this->form_validation->set_rules("user_name","Kullanıcı Adı","required|trim|is_unique[users.user_name]");
         }
+
         if($oldUser->email != $this->input->post("email")){
             $this->form_validation->set_rules("email","E-posta","required|trim|valid_email|is_unique[users.email]");
         }
--- panel/application/views/settings_v/add/content.php
+++ panel/application/views/settings_v/add/content.php
@@ -1,10 +1,10 @@
 <div class="row">
 	<div class="col-md-12">
 		<h4 class="m-b-lg">
-			Yeni Kullanıcı Ekle
+			Site Ayarı Ekle
 		</h4>
 		<div class="col-md-12">
-		<form action="<?php echo base_url("users/save");?>" method="post">
+		<form action="<?php echo base_url("settings/save");?>" method="post" enctype="multipart/form-data">
 				<div class="widget">
 					<div class="m-b-lg nav-tabs-horizontal">
 						<!-- tabs list -->
@@ -41,26 +41,17 @@
 										</div>
 										<div class="form-group col-md-4">
 											<label>Telefon 2</label>
-											<input class="form-control" placeholder="Diğer telefon numaranız (opsiyonel)" name="phone_2" value = "<?php echo isset($form_error) ? set_value("phone_2") : ""; ?>">
-											<?php if(isset($form_error)){ ?>
-												<small class="pull-right input-form-error"><?php echo form_error("phone_2"); ?></small>
-											<?php } ?>	
+											<input class="form-control" placeholder="Diğer telefon numaranız (opsiyonel)" name="phone_2">
 										</div>
 									</div>
 									<div class="row">
 										<div class="form-group col-md-4">
 											<label>Faks 1</label>
-											<input class="form-control" placeholder="Faks numaranız" name="fax_1" value = "<?php echo isset($form_error) ? set_value("fax_1") : ""; ?>">
-											<?php if(isset($form_error)){ ?>
-												<small class="pull-right input-form-error"><?php echo form_error("fax_1"); ?></small>
-											<?php } ?>	
+											<input class="form-control" placeholder="Faks numaranız" name="fax_1">
 										</div>
 										<div class="form-group col-md-4">
 											<label>Faks 2</label>
-											<input class="form-control" placeholder="Diğer faks numaranız (opsiyonel)" name="fax_2" value = "<?php echo isset($form_error) ? set_value("fax_2") : ""; ?>">
-											<?php if(isset($form_error)){ ?>
-												<small class="pull-right input-form-error"><?php echo form_error("fax_2"); ?></small>
-											<?php } ?>	
+											<input class="form-control" placeholder="Diğer faks numaranız (opsiyonel)" name="fax_2">
 										</div>
 									</div>
 								</div><!-- .tab-pane  -->
@@ -119,18 +110,12 @@
 												<input class="form-control" placeholder="Facebook Adresiniz" 
 													name="facebook" 
 													value = "<?php echo isset($form_error) ? set_value("facebook") : ""; ?>">
-												<?php if(isset($form_error)){ ?>
-												<small class="pull-right input-form-error"><?php echo form_error("facebook"); ?></small>
-												<?php } ?>	
 											</div>
 											<div class="form-group col-md-4">
 												<label>Twitter</label>
 												<input class="form-control" placeholder="Twitter Adresiniz" 
 												name="twitter" 
 												value = "<?php echo isset($form_error) ? set_value("twitter") : ""; ?>">
-												<?php if(isset($form_error)){ ?>
-													<small class="pull-right input-form-error"><?php echo form_error("twitter"); ?></small>
-												<?php } ?>	
 											</div>
 										</div>
 										<div class="row">
@@ -139,18 +124,12 @@
 												<input class="form-control" placeholder="Instagram Adresiniz" 
 													name="instagram" 
 													value = "<?php echo isset($form_error) ? set_value("instagram") : ""; ?>">
-												<?php if(isset($form_error)){ ?>
-												<small class="pull-right input-form-error"><?php echo form_error("instagram"); ?></small>
-												<?php } ?>	
 											</div>
 											<div class="form-group col-md-4">
 												<label>LinkedIn</label>
 												<input class="form-control" placeholder="LinkedIn Adresiniz" 
 												name="linkedin" 
 												value = "<?php echo isset($form_error) ? set_value("linkedin") : ""; ?>">
-												<?php if(isset($form_error)){ ?>
-													<small class="pull-right input-form-error"><?php echo form_error("linkedin"); ?></small>
-												<?php } ?>	
 											</div>
 										</div>
 								</div><!-- .tab-pane  -->
@@ -165,10 +144,11 @@
 								</div><!-- .tab-pane  -->
 							</div><!-- .tab-content  -->
 							
-						</form>
+						
 					</div><!-- .nav-tabs-horizontal -->
 				</div><!-- .widget -->
 				<button type="submit" class="btn btn-primary btn-md">Kaydet</button>
-							<a href="<?php echo base_url("users");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+				<a href="<?php echo base_url("settings");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+				</form>
 			</div><!-- END column -->
 </div>
