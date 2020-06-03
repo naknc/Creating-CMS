efa452328850e44d0ea1e54156d3706c42c3cf54
naknc <nihan.akinci.35@gmail.com>
2020-06-03 12:33:07 +0300

    Yeni portfolyo sayfasının tasarımı.

--- panel/application/views/portfolio_v/add/content.php
+++ panel/application/views/portfolio_v/add/content.php
@@ -7,20 +7,75 @@
 	<div class="col-md-12">
 	<div class="widget">
 		<div class="widget-body">
-			<form action="<?php echo base_url("product/save");?>" method="post">
-				<div class="form-group">
-					<label>Başlık</label>
-					<input class="form-control" placeholder="Başlık" name="title">
-					<?php if(isset($form_error)){ ?>
-						<small class="pull-right input-form-error"><?php echo form_error("title"); ?></small>
-					<?php } ?>	
+			<form action="<?php echo base_url("portfolio/save");?>" method="post">
+				<div class="row">
+					<div class="form-group col-md-6">
+						<label>Başlık</label>
+						<input class="form-control" placeholder="İşi anlatan başlık bilgisi" name="title">
+						<?php if(isset($form_error)){ ?>
+							<small class="pull-right input-form-error"><?php echo form_error("title"); ?></small>
+						<?php } ?>	
+					</div>
+					<div class="form-group col-md-6">
+						<label>Kategori</label>
+							<select name="category_id" class="form-control">
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+							</select>
+							<?php if(isset($form_error)){ ?>
+								<small class="pull-right input-form-error"><?php echo form_error("client"); ?></small>
+							<?php } ?>	
+					</div>
 				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<label for="datetimepicker1">Bitirme Tarihi</label>
+						<input type="hidden" name="finishedAt" id="datetimepicker1" 
+							data-plugin="datetimepicker" 
+							data-options="{ inline: true, viewMode: 'days', format : 'YYYY-MM-DD HH:mm:ss' }">
+					</div>
+					<div class="col-md-8">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<label>Müşteri</label>
+									<input class="form-control" placeholder="İşi yaptığınız müşteri" name="client">
+									<?php if(isset($form_error)){ ?>
+										<small class="pull-right input-form-error"><?php echo form_error("client"); ?></small>
+									<?php } ?>	
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<label>Yer/Mekan</label>
+									<input class="form-control" placeholder="İşi yaptığınız yer, mekan bilgisi" name="place">
+									<?php if(isset($form_error)){ ?>
+										<small class="pull-right input-form-error"><?php echo form_error("place"); ?></small>
+									<?php } ?>	
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<label>Yapılan işin Bağlantısı (URL)</label>
+									<input class="form-control" placeholder="Yapılan işin internet üzerindeki bağlantısı" name="portfolio_url">
+									<?php if(isset($form_error)){ ?>
+										<small class="pull-right input-form-error"><?php echo form_error("portfolio_url"); ?></small>
+									<?php } ?>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
 				<div class="form-group">
 					<label>Açıklama</label>
 					<textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
 				</div>
 				<button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
-				<a href="<?php echo base_url("product");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+				<a href="<?php echo base_url("portfolio");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
 			</form>
 		</div><!-- .widget-body -->
 	</div><!-- .widget -->
